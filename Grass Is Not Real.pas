06825c535ff6470cea0c8559656d70ee19ddf2a3
DoubleYouC <66910033+DoubleYouC@users.noreply.github.com>
2025-08-06 23:22:36 -0400

    Main concept done for Grass Is Not Real

--- Grass Is Not Real.pas
+++ Grass Is Not Real.pas
@@ -6,12 +6,14 @@ unit MakeGrassNotReal;
 var
     joGrass: TJsonObject;
     plugin: IwbFile;
-    statGroup: IwbGroupRecord;
+    statGroup, scolGroup: IwbGroupRecord;
     slPluginFiles: TStringList;
+    sIgnoredWorldspaces: string;
 
 function Initialize: integer;
 begin
     joGrass := TJsonObject.Create;
+    sIgnoredWorldspaces := '000F93:Fallout4.esm,000810:DLCCoast.esm,0014F4:DLCCoast.esm,008B56:DLCNukaWorld.esm';
 
     CreatePluginList;
     CreatePlugin;
@@ -22,7 +24,6 @@ end;
 
 function Finalize: integer;
 begin
-    AddMessage(joGrass.ToJSON(True));
     joGrass.Free;
     slPluginFiles.Free;
     Result := 0;
@@ -44,7 +45,9 @@ end;
 procedure CreatePlugin;
 begin
     plugin := AddNewFile;
+    AddMasterIfMissing(plugin, 'Fallout4.esm');
     statGroup := Add(plugin, 'STAT', True);
+    scolGroup := Add(plugin, 'SCOL', True);
     slPluginFiles.Add(GetFileName(plugin));
 end;
 
@@ -53,48 +56,220 @@ var
     i, j, k, idx: integer;
     f: IwbFile;
     g: IwbGroupRecord;
-    r, gnams, gnam: IwbElement;
-    recordId, grassRecordId, staticGrassRecordId: string;
-    slLtex, slGrasses: TStringList;
-    tlLtex: TList;
+    r, gnams, gnam, land: IwbElement;
+    recordId, grassRecordId, staticGrassRecordId, landRecordId: string;
+    slLtex, slLand: TStringList;
+    tlLtex, tlLand: TList;
 begin
 
     slLtex := TStringList.Create;
     tlLtex := TList.Create;
+    slLand := TStringList.Create;
+    tlLand := TList.Create;
     try
         for i := 0 to Pred(FileCount) do begin
             f := FileByIndex(i);
 
             g := GroupBySignature(f, 'LTEX');
 
+            // iterate over Landscape Textures
             for j := 0 to Pred(ElementCount(g)) do begin
                 r := WinningOverride(ElementByIndex(g, j));
                 recordId := RecordFormIdFileId(r);
                 idx := slLtex.IndexOf(recordId);
                 if idx > -1 then continue
                 slLtex.Add(recordId);
+
+                // Check for grass
                 if not ElementExists(r, 'GNAM') then continue;
                 tlLtex.Add(r);
-                AddMessage(ShortName(r));
+                //AddMessage(ShortName(r));
+
+
                 gnams := ElementByPath(r, 'GNAM');
+                // Iterate over grasses
                 for k := 0 to Pred(ElementCount(gnams)) do begin
                     gnam := WinningOverride(LinksTo(ElementByIndex(gnams, k)));
-                    AddMessage(#9 + ShortName(gnam));
+                    //AddMessage(#9 + ShortName(gnam));
                     grassRecordId := RecordFormIdFileId(gnam);
                     joGrass.O['Landscape Textures'].O[recordId].A['Grasses'].Add(grassRecordId);
                     staticGrassRecordId := RecordFormIdFileId(GrassStatic(gnam));
                     joGrass.O['Landscape Textures'].O[recordId].A['StaticGrasses'].Add(staticGrassRecordId);
                 end;
+
+                // Iterate over ReferencedBy to get LAND records
+                for k := Pred(ReferencedByCount(r)) downto 0 do begin
+                    land := WinningOverride(ReferencedByIndex(r, k));
+                    if Signature(land) <> 'LAND' then continue;
+                    landRecordId := RecordFormIdFileId(land);
+                    idx := slLand.IndexOf(landRecordId);
+                    if idx > -1 then continue;
+                    slLand.Add(landRecordId);
+                    tlLand.Add(land);
+                    joGrass.O['Cells'].O[landRecordId].A['Landscape Textures'].Add(recordId);
+                end;
             end;
         end;
 
-        for i := 0 to Pred(tlLtex.Count) do begin
-            r := ObjectToElement(tlLtex[i]);
+        // Now that we have all the LAND records that should get grass, we will iterate over them and add static grass to them.
+        for i := 0 to Pred(tlLand.Count) do begin
+            land := ObjectToElement(tlLand[i]);
+            AddStaticGrassToLand(land);
         end;
     finally
         slLtex.Free;
         tlLtex.Free;
+        slLand.Free;
+        tlLand.Free;
     end;
+end;
+
+function AddStaticGrassToLand(land: IwbElement): integer;
+var
+    rCell, rWrld, nCell, landHeightData, grassSCOL, grassSCOLRef: IwbElement;
+    cellX, cellY, unitsX, unitsY, row, column: integer;
+    wrldLandZ, landOffsetZ, landHeightZ, rowColumnOffsetZ: double;
+    rowColumn, pX, pY, pZ, rX, rY, rZ, scale, grassSCOLFormid, landRecordId, grassHere: string;
+    joGrassSCOL: TJsonObject;
+begin
+    rCell := WinningOverride(LinksTo(ElementByIndex(land, 0)));
+    rWrld := WinningOverride(LinksTo(ElementByIndex(rCell, 0)));
+    if Pos(RecordFormIdFileId(rWrld), sIgnoredWorldspaces) <> 0 then Exit;
+    cellX := GetElementNativeValues(rCell, 'XCLC\X');
+    cellY := GetElementNativeValues(rCell, 'XCLC\Y');
+    unitsX := cellX * 4096;
+    unitsY := cellY * 4096;
+
+    wrldLandZ := GetElementNativeValues(rWrld, 'DNAM\Default Land Height');
+    landOffsetZ := GetElementNativeValues(land, 'VHGT\Offset');
+    landHeightZ := wrldLandZ + landOffsetZ;
+
+    landHeightData := ElementByPath(land, 'VHGT\Height Data');
+
+    landRecordId := RecordFormIdFileId(land);
+
+    joGrassSCOL := TJsonObject.Create;
+    try
+        for row := 0 to 32 do begin
+            for column := 0 to 32 do begin
+                rowColumn := 'Row #' + IntToStr(row) + '\Column #' + IntToStr(column);
+                rowColumnOffsetZ := GetElementNativeValues(landHeightData, rowColumn);
+                pX := FloatToStr(unitsX + (column * 128));
+                pY := FloatToStr(unitsY + (row * 128));
+                pZ := FloatToStr(rowColumnOffsetZ);
+
+                rx := '0.0';
+                rY := '0.0';
+                rZ := '0.0';
+                scale := '1.0';
+
+                grassHere := GetRandomGrass(landRecordId);
+                joGrassSCOL.O[grassHere].A['Placements'].Add(pX + ',' + pY + ',' + pZ + ',' + rX + ',' + rY + ',' + rZ + ',' + scale);
+            end;
+        end;
+        grassSCOL := MakeSCOLFromJson(joGrassSCOL, 'GrassSCOL_' + EditorID(rWrld) + '_' + IntToStr(cellX) + '_' + IntToStr(cellY));
+    finally
+        joGrassSCOL.Free;
+    end;
+    AddRequiredElementMasters(rWrld, plugin, False, True);
+    AddRequiredElementMasters(rCell, plugin, False, True);
+  	SortMasters(plugin);
+    wbCopyElementToFile(rWrld, plugin, False, True);
+    nCell := wbCopyElementToFile(rCell, plugin, False, True);
+    grassSCOLRef := Add(nCell, 'REFR', True);
+    grassSCOLFormid := IntToHex(GetLoadOrderFormID(grassSCOL), 8);
+    SetElementEditValues(grassSCOLRef, 'Name', grassSCOLFormid);
+    SetElementEditValues(grassSCOLRef, 'DATA\Position\X', IntToStr(unitsX));
+    SetElementEditValues(grassSCOLRef, 'DATA\Position\Y', IntToStr(unitsY));
+    SetElementEditValues(grassSCOLRef, 'DATA\Position\Z', FloatToStr(landHeightZ));
+
+    Result := 0;
+end;
+
+function MakeSCOLFromJson(joSCOL: TJsonObject; sSCOLEditorId: string): IwbElement;
+{
+    Creates a SCOL record from the given JSON object.
+    The JSON object should have the structure:
+
+        "BaseRecordId":
+            "Placements": [
+                "pX,pY,pZ,rX,rY,rZ,scale",
+                ...
+            ]
+}
+var
+    a, c, n, DelimPos, t: integer;
+    placementValue, Token, key: string;
+    scol, stat, parts, part, onam, placement, placements: IwbElement;
+begin
+    //Add SCOL record to SCOL group
+    scol := Add(scolGroup, 'SCOL', True);
+    SetEditorID(scol, sSCOLEditorId);
+
+    //Add Parts
+    parts := Add(scol, 'Parts', True);
+    part := ElementbyIndex(parts, 0);
+    onam := ElementByPath(part, 'ONAM');
+    SetEditValue(onam, 'StaticCollectionPivotDummy [STAT:00035812]');
+
+    t := 0;
+    for c := 0 to Pred(joSCOL.Count) do begin
+        key := joSCOL.Names[c];
+        stat := GetRecordFromFormIdFileId(key);
+        //AddMessage(key);
+
+        // Add ONAM for each key (base STAT)
+        part := Add(parts, 'Part', True);
+        onam := ElementByPath(part, 'ONAM');
+        SetEditValue(onam, IntToHex(GetLoadOrderFormID(stat), 8));
+
+        placements := Add(part, 'DATA', True);
+        for a := 0 to Pred(joSCOL.O[key].A['Placements'].Count) do begin
+            t := t + 1;
+            placement := Add(placements, 'Placement', True);
+            // Add Placement for each placement in the key
+            placementValue := joSCOL.O[key].A['Placements'].S[a];
+            //AddMessage(#9 + 'Placement: ' + placementValue);
+            n := 0;
+            while placementValue <> '' do begin
+                DelimPos := Pos(',', placementValue);
+                if DelimPos > 0 then begin
+                    Token := Copy(placementValue, 1, DelimPos - 1);
+                    Delete(placementValue, 1, DelimPos);
+                end
+                else begin
+                    Token := placementValue;
+                    placementValue := '';
+                end;
+                n := n + 1;
+
+                Case n of
+                    1 : SetElementEditValues(placement, 'Position\X', Token);
+                    2 : SetElementEditValues(placement, 'Position\Y', Token);
+                    3 : SetElementEditValues(placement, 'Position\Z', Token);
+                    4 : SetElementEditValues(placement, 'Rotation\X', Token);
+                    5 : SetElementEditValues(placement, 'Rotation\Y', Token);
+                    6 : SetElementEditValues(placement, 'Rotation\Z', Token);
+                    7 : SetElementEditValues(placement, 'Scale', Token);
+                end;
+            end;
+        end;
+    end;
+    //AddMessage('Total placements: ' + IntToStr(t));
+end;
+
+function GetRandomGrass(landRecordId: string): string;
+{
+    Returns a random static grass record id for the given LAND record.
+}
+var
+    ltexIdx, grassIdx: integer;
+    ltex: string;
+begin
+    ltexIdx := Random(joGrass.O['Cells'].O[landRecordId].A['Landscape Textures'].Count);
+    ltex := joGrass.O['Cells'].O[landRecordId].A['Landscape Textures'].S[ltexIdx];
+    grassIdx := Random(joGrass.O['Landscape Textures'].O[ltex].A['StaticGrasses'].Count);
+    Result := joGrass.O['Landscape Textures'].O[ltex].A['StaticGrasses'].S[grassIdx];
 end;
 
 function GrassStatic(grassRecord: IwbElement): IwbElement;
@@ -145,7 +320,7 @@ end;
 
 function RecordFormIdFileId(e: IwbElement): string;
 {
-    Returns the record ID of an element.
+    Returns the record ID of an element as formid:filename.
 }
 begin
     Result := TrimRightChars(IntToHex(FixedFormID(e), 8), 2) + ':' + GetFileName(GetFile(MasterOrSelf(e)));
@@ -153,7 +328,7 @@ end;
 
 function GetRecordFromFormIdFileId(recordId: string): IwbElement;
 {
-    Returns the record from the given record ID.
+    Returns the record from the given formid:filename.
 }
 var
     colonPos, recordFormId: integer;
